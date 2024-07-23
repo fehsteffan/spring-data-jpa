e75d04ab190ca55ccf4e888a889e94763af4f75d
Felipe Steffan <felip.developer@hotmail.com>
2024-07-23 11:46:28 -0300

    Relacionamentos OneToMany / ManyTomany

--- src/main/java/com/bookstore/jpa/model/AuthorModel.java
+++ src/main/java/com/bookstore/jpa/model/AuthorModel.java
@@ -1,8 +1,11 @@
 package com.bookstore.jpa.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 
@@ -17,6 +20,10 @@ public class AuthorModel implements Serializable {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToMany(mappedBy = "authors", fetch = FetchType.LAZY)
+    private Set<BookModel> books = new HashSet<>();
 
     public AuthorModel() {
 
@@ -41,5 +48,13 @@ public class AuthorModel implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<BookModel> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<BookModel> books) {
+        this.books = books;
     }
 }
--- src/main/java/com/bookstore/jpa/model/BookModel.java
+++ src/main/java/com/bookstore/jpa/model/BookModel.java
@@ -1,14 +1,16 @@
 package com.bookstore.jpa.model;
 
 
-import ch.qos.logback.core.boolex.EvaluationException;
+
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tb_model")
+@Table(name = "TB_BOOK")
 public class BookModel implements Serializable {
     private static final long serialVerisonUID = 1l;
 
@@ -18,6 +20,22 @@ public class BookModel implements Serializable {
 
     @Column(nullable = false, unique = true)
     private String title;
+
+
+    @ManyToOne
+    @JoinColumn(name = "publisher_id")
+    private PublisherModel publisher;
+
+    @OneToMany
+    @JoinTable(name = "tb_book_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<AuthorModel> authors = new HashSet<>();
+
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL)
+    private ReviewModel review;
+
+
 
     public BookModel() {
 
@@ -40,7 +58,31 @@ public class BookModel implements Serializable {
         return title;
     }
 
+    public PublisherModel getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(PublisherModel publisher) {
+        this.publisher = publisher;
+    }
+
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Set<AuthorModel> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<AuthorModel> authors) {
+        this.authors = authors;
+    }
+
+    public ReviewModel getReview() {
+        return review;
+    }
+
+    public void setReview(ReviewModel review) {
+        this.review = review;
     }
 }
